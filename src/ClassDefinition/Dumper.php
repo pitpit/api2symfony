625942a64b0dd74834c3bbf80a126d3be6d52231
Damien <d.pitard@creads.org>
2015-02-16 19:54:45 +0100

    removed dependency with mondator (unstable)

---
+++ src/ClassDefinition/Dumper.php
@@ -0,0 +1,305 @@
+<?php
+
+namespace Creads\Api2Symfony\ClassDefinition;
+
+/**
+ * The dumper.
+ *
+ * This file was part of Mondator (https://github.com/mandango/mondator)
+ * (c) Pablo Díez <pablodip@gmail.com>
+ * See LICENSE file
+ *
+ * @author Pablo Díez <pablodip@gmail.com>
+ * @author Damien Pitard <damien.pitard@gmail.com>
+ */
+class Dumper
+{
+    const INDENT_SPACES = 4;
+
+    protected $definition;
+
+    /**
+     * Constructor.
+     *
+     * @param Definition $definition The definition.
+     *
+     * @api
+     */
+    public function __construct(Definition $definition)
+    {
+        $this->setDefinition($definition);
+    }
+
+    /**
+     * Set the definition.
+     *
+     * @param Definition $definition The definition.
+     *
+     * @api
+     */
+    public function setDefinition(Definition $definition)
+    {
+        $this->definition = $definition;
+    }
+
+    /**
+     * Returns the definition
+     *
+     * @return Definition The definition.
+     *
+     * @api
+     */
+    public function getDefinition()
+    {
+        return $this->definition;
+    }
+
+    /**
+     * Dump the definition.
+     *
+     * @return string The PHP code of the definition.
+     *
+     * @api
+     */
+    public function dump()
+    {
+        return
+            $this->startFile().
+            $this->addNamespace().
+            $this->addUses().
+            $this->startClass().
+            $this->addProperties().
+            $this->addMethods().
+            $this->endClass()
+        ;
+    }
+
+    /**
+     * Export an array.
+     *
+     * Based on Symfony\Component\DependencyInjection\Dumper\PhpDumper::exportParameters
+     * http://github.com/symfony/symfony
+     *
+     * @param array $array  The array.
+     *
+     * @return string The array exported.
+     */
+    public static function renderArray(array $array, $tabs = 0)
+    {
+        // return var_export($array, true);
+
+        if (count($array)) {
+
+            $code = array();
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    $value = self::exportArray($value, $tabs + 1);
+                } else {
+                    $value = null === $value ? 'null' : var_export($value, true);
+                }
+
+                $code[] = sprintf('%s%s => %s,', str_repeat(' ', ($tabs+1) * self::INDENT_SPACES), var_export($key, true), $value);
+            }
+
+            return sprintf("array(\n%s\n%s)", implode("\n", $code), str_repeat(' ', $tabs * self::INDENT_SPACES));
+        } else {
+            return 'array()';
+        }
+    }
+
+
+    /**
+     * Indents code with tabs (4 spaces)
+     *
+     * @return string
+     */
+    public static function renderIndent($code, $tabs = 0)
+    {
+        $output = '';
+        $lines = preg_split('/\r\n|\r|\n/', $code);
+        $count = count($lines);
+        foreach ($lines as $i => $line) {
+            $line = rtrim($line);
+            if (!empty($line)) {
+                for ($j = 0; $j < $tabs; $j++) {
+                    $output .= '    ';
+                }
+                $output .= $line;
+            }
+            if ($i < ($count - 1)) {
+                $output .= "\n";
+            }
+        }
+        return $output;
+    }
+
+    protected function startFile()
+    {
+        return <<<EOF
+<?php
+
+EOF;
+    }
+
+    protected function addNamespace()
+    {
+        if (!$namespace = $this->definition->getNamespace()) {
+            return '';
+        }
+
+        return <<<EOF
+
+namespace $namespace;
+
+EOF;
+    }
+
+    protected function addUses()
+    {
+        $code = '';
+        $uses = $this->definition->getUses();
+        if (count($uses)) {
+            $code .= "\n";
+
+            foreach ($uses as $class => $alias) {
+                $code .= "use $class";
+                if ($alias) {
+                    $code .= "as $alias";
+                }
+                $code .= ";\n";
+            }
+        }
+
+        return $code;
+    }
+
+    protected function startClass()
+    {
+        $code = "\n";
+
+        // doc comment
+        if ($docComment = $this->definition->getDocComment()) {
+            $code .= $docComment."\n";
+        }
+
+        /*
+         * declaration
+         */
+        $declaration = '';
+
+        // abstract
+        if ($this->definition->isAbstract()) {
+            $declaration .= 'abstract ';
+        }
+
+        // class
+        $declaration .= 'class '.$this->definition->getClassName();
+
+        // parent class
+        if ($parentClass = $this->definition->getParentClass()) {
+            $declaration .= ' extends '.$parentClass;
+        }
+
+        // interfaces
+        if ($interfaces = $this->definition->getInterfaces()) {
+            $declaration .= ' implements '.implode(', ', $interfaces);
+        }
+
+        $code .= <<<EOF
+$declaration
+{
+EOF;
+
+        return $code;
+    }
+
+    protected function addProperties()
+    {
+        $code = '';
+
+        $properties = $this->definition->getProperties();
+        foreach ($properties as $property) {
+            $code .= "\n";
+
+            if ($docComment = $property->getDocComment()) {
+                $code .= $docComment."\n";
+            }
+            $isStatic = $property->isStatic() ? 'static ' : '';
+
+            $value = $property->getValue();
+            if (null === $value) {
+                $code .= <<<EOF
+    $isStatic{$property->getVisibility()} \${$property->getName()};
+EOF;
+            } else {
+                $value = is_array($property->getValue()) ? self::renderArray($property->getValue()) : var_export($property->getValue(), true);
+
+                $code .= <<<EOF
+    $isStatic{$property->getVisibility()} \${$property->getName()} = $value;
+EOF;
+            }
+        }
+        if ($properties) {
+            $code .= "\n";
+        }
+
+        return $code;
+    }
+
+    protected function addMethods()
+    {
+        $code = '';
+
+        foreach ($this->definition->getMethods() as $method) {
+            $code .= "\n";
+
+            // doc comment
+            if ($docComment = $method->getDocComment()) {
+                $code .= self::renderIndent($docComment, 1)."\n";
+            }
+
+            // isFinal
+            $isFinal = $method->isFinal() ? 'final ' : '';
+
+            // isStatic
+            $isStatic = $method->isStatic() ? 'static ' : '';
+
+            // abstract
+            if ($method->isAbstract()) {
+                $code .= <<<EOF
+    abstract $isStatic{$method->getVisibility()} function {$method->getName()}({$method->getArguments()});
+EOF;
+            } else {
+                $methodCode = $method->getCode();
+                if ($methodCode) {
+                    $methodCode = (($method->getAutoIndentation())?self::renderIndent($methodCode, 1):$methodCode)."\n";
+                }
+                $code .= <<<EOF
+    $isFinal$isStatic{$method->getVisibility()} function {$method->getName()}({$method->getArguments()})
+    {
+{$methodCode}
+    }
+EOF;
+            }
+            $code =  $code . "\n";
+        }
+
+        return $code;
+    }
+
+    protected function endClass()
+    {
+        $code = '';
+
+        if (!$this->definition->getProperties() && !$this->definition->getMethods()) {
+            $code .= "\n";
+        }
+
+        $code .= <<<EOF
+}
+
+EOF;
+
+        return $code;
+    }
+}
---
+++ src/ClassDefinition/Method.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace Creads\Api2Symfony\ClassDefinition;
+
+/**
+ * Represents a method of a class.
+ *
+ * This file was part of Mondator (https://github.com/mandango/mondator)
+ * (c) Pablo Díez <pablodip@gmail.com>
+ * See LICENSE file
+ *
+ * @author Pablo Díez <pablodip@gmail.com>
+ * @author Damien Pitard <damien.pitard@gmail.com>
+ */
+class Method
+{
+    protected $visibility;
+    protected $name;
+    protected $arguments;
+    protected $code;
+    protected $final;
+    protected $static;
+    protected $abstract;
+    protected $docComment;
+    protected $autoIndentation;
+
+    /**
+     * Constructor.
+     *
+     * @param string $visibility The visibility.
+     * @param string $name       The name.
+     * @param string $arguments  The arguments (as string).
+     * @param string $code       The code.
+     *
+     * @api
+     */
+    public function __construct($visibility, $name, $arguments, $code)
+    {
+        $this->setVisibility($visibility);
+        $this->setName($name);
+        $this->setArguments($arguments);
+        $this->setCode($code);
+        $this->final = false;
+        $this->static = false;
+        $this->abstract = false;
+        $this->autoIndentation = true;
+    }
+
+    /**
+     * Set the visibility.
+     *
+     * @param string $visibility The visibility.
+     *
+     * @api
+     */
+    public function setVisibility($visibility)
+    {
+        $this->visibility = $visibility;
+    }
+
+    /**
+     * Returns the visibility.
+     *
+     * @return string The visibility.
+     *
+     * @api
+     */
+    public function getVisibility()
+    {
+        return $this->visibility;
+    }
+
+    /**
+     * Set the name.
+     *
+     * @param string $name The name.
+     *
+     * @api
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Returns the name.
+     *
+     * @return string The name.
+     *
+     * @api
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the arguments.
+     *
+     * Example: "$argument1, &$argument2"
+     *
+     * @param string $arguments The arguments (as string).
+     *
+     * @api
+     */
+    public function setArguments($arguments)
+    {
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * Returns the arguments.
+     *
+     * @api
+     */
+    public function getArguments()
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * Set the code.
+     *
+     * @param string $code.
+     *
+     * @api
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * Returns the code.
+     *
+     * @return string The code.
+     *
+     * @api
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set if the method is final.
+     *
+     * @param bool $final If the method is final.
+     *
+     * @api
+     */
+    public function setFinal($final)
+    {
+        $this->final = (bool) $final;
+    }
+
+    /**
+     * Returns if the method is final.
+     *
+     * @return bool If the method is final.
+     *
+     * @api
+     */
+    public function isFinal()
+    {
+        return $this->final;
+    }
+
+    /**
+     * Set if the method is static.
+     *
+     * @param bool $static If the method is static.
+     *
+     * @api
+     */
+    public function setStatic($static)
+    {
+        $this->static = (bool) $static;
+    }
+
+    /**
+     * Return if the method is static.
+     *
+     * @return bool Returns if the method is static.
+     *
+     * @api
+     */
+    public function isStatic()
+    {
+        return $this->static;
+    }
+
+    /**
+     * Set if the method is abstract.
+     *
+     * @param bool $abstract If the method is abstract.
+     *
+     * @api
+     */
+    public function setAbstract($abstract)
+    {
+        $this->abstract = (bool) $abstract;
+    }
+
+    /**
+     * Return if the method is abstract.
+     *
+     * @return bool Returns if the method is abstract.
+     *
+     * @api
+     */
+    public function isAbstract()
+    {
+        return $this->abstract;
+    }
+
+    /**
+     * Set the doc comment.
+     *
+     * @param string|null $docComment The doc comment.
+     *
+     * @api
+     */
+    public function setDocComment($docComment)
+    {
+        $this->docComment = $docComment;
+    }
+
+    /**
+     * Returns the doc comment.
+     *
+     * @return string|null The doc comment.
+     *
+     * @api
+     */
+    public function getDocComment()
+    {
+        return $this->docComment;
+    }
+
+    /**
+     * Gets the value of autoIndentation.
+     *
+     * @return mixed
+     */
+    public function getAutoIndentation()
+    {
+        return $this->autoIndentation;
+    }
+
+    /**
+     * Disable auto-indentation of the code (usefull when you need to stick code to the left)
+     *
+     * @param mixed $autoIndentation the auto indentation
+     *
+     * @return self
+     */
+    public function setAutoIndentation($autoIndentation)
+    {
+        $this->autoIndentation = $autoIndentation;
+
+        return $this;
+    }
+}
